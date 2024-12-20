dd87794ee7f8fcda6ff7327cb1b78bc83a07d6b6
Sebastián Martinez <sebamar88@gmail.com>
2024-12-20 16:25:32 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+Console.WriteLine("Hello, World!");
+
+int numero = 1;
+string numeroString = "1";
+bool resultado = int.TryParse(numeroString, out numero);
+float numeroFloat = (float)(numero + 1.5);
+char letra = 'a';
+double numeroDouble = 0 + 1.5;
+long numeroLong = 08694564654564648989;
+uint numeroUInt = 4294967295;
